76fcaf4cafb60afac783e1594785ff14ab48f410
Akigaga <nishi.lekhi@gmail.com>
2026-03-20 17:11:55 -0400

    test

--- LOOPS/nestedloopthree.py
+++ LOOPS/nestedloopthree.py
@@ -3,9 +3,13 @@ upper = int(input("enter a upper range: "))
 
 print("Prime number between", lower, "and", upper, "are:")
 for num in range(lower, upper + 1):
+    print("current number is", num)
     if num > 1:
         for i in range(2, num):
+            print("current divisor is", i)
             if(num % i) == 0:
+                print(num, "is not a prime number")
                 break
         else:
-            print(num)
+            print("current prime number is", num)
+    print("-----------------------------")
